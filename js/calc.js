79bafb6cf94dc051f08348a1dfcb0da6f8c3cc0b
Daniel Milkiewicz <daniel.milkiewicz@outlook.com>
2018-03-13 20:29:31 +0100

    Complete version

--- js/calc.js
+++ js/calc.js
@@ -8,7 +8,7 @@ let divide = document.getElementById("divide");
 let result = document.getElementById("result");
 let display = document.getElementById("display");
 let displayValue;
-let operationValue;
+let flag;
 
 for(let button of buttons){
     button.addEventListener("click", function(){
@@ -17,10 +17,43 @@ for(let button of buttons){
 }
 
 add.addEventListener("click", function(){
-    displayValue = +display.textContent;
-    display.textContent = null;
+    operation();
+    flag = "add";
+});
+
+substract.addEventListener("click", function(){
+    operation();
+    flag = "substract";
+});
+
+multiply.addEventListener("click", function(){
+    operation();
+    flag = "multiply";
+});
+
+divide.addEventListener("click", function(){
+    operation();
+    flag = "divide";
 });
 
 result.addEventListener("click", function(){
-    display.textContent = ((displayValue * 10) + (display.textContent * 10)) / 10;
+    switch(flag){
+        case "add": 
+            display.textContent = ((displayValue * 10) + (display.textContent * 10)) / 10;
+            break;
+        case "substract": 
+            display.textContent = ((displayValue * 10) - (display.textContent * 10)) / 10;
+            break;
+        case "multiply": 
+            display.textContent = ((displayValue * 10) * (display.textContent * 10)) / 100;
+            break;
+        case "divide": 
+            display.textContent = ((displayValue * 10) / (display.textContent * 10));
+            break;
+    }
 });
+
+function operation(){
+    displayValue = +display.textContent;
+    display.textContent = null;
+}
