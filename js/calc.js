f9060266b551b490cdac728ab19bbf4683803383
Daniel Milkiewicz <daniel.milkiewicz@outlook.com>
2018-03-13 19:50:08 +0100

    Stage release

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,26 @@
 "use strict";
 
 let buttons = document.getElementsByClassName("button");
+let add = document.getElementById("add");
+let substract = document.getElementById("substract");
+let multiply = document.getElementById("multiply");
+let divide = document.getElementById("divide");
+let result = document.getElementById("result");
+let display = document.getElementById("display");
+let displayValue;
+let operationValue;
 
-for(let i = 0; i <= buttons.length; i++){
-    document.getElementsByClassName("button")[i].addEventListener("click", function(){
-        document.getElementById("display").textContent = "test";
-    });
+for(let button of buttons){
+    button.addEventListener("click", function(){
+        display.textContent += button.value;
+    }, false);
 }
+
+add.addEventListener("click", function(){
+    displayValue = +display.textContent;
+    display.textContent = null;
+});
+
+result.addEventListener("click", function(){
+    display.textContent = ((displayValue * 10) + (display.textContent * 10)) / 10;
+});
